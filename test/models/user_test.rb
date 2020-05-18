7271234349b35124284acc806e031fe518a9ead5
Marco Montis <marcmontis7@gmail.com>
2020-05-18 21:33:44 +0200

    add 5 tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "full_name returns capitalized first name and last name" do
+    user = User.new(first_name: "John", last_name: "Doe")
+    assert_equal "John Doe", user.full_name
+  end
+
+  test "do not crash if first_name is not provided" do
+    user = User.new(last_name: "Doe")
+    assert_equal "Doe", user.full_name
+  end
 end
--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -3,7 +3,40 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do
     visit "/"
+    # save_and_open_screenshot
 
     assert_selector "h1", text: "Product"
+    assert_selector ".card-product-infos", count: Product.count
+  end
+
+  test "lets a signed in user create a new product" do
+    login_as users(:george)
+    visit "/products/new"
+    # save_and_open_screenshot
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+    # save_and_open_screenshot
+
+    click_on 'Create Product'
+    # save_and_open_screenshot
+
+    # Should be redirected to Home with new product
+    assert_equal root_path, page.current_path
+    assert_text "Change your life: Learn to code"
+  end
+
+  test "prevent user from creating a product with an empty name" do
+    login_as users(:george)
+    visit "/products/new"
+
+    # exercise
+    product_count_before = Product.count
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+    click_on 'Create Product'
+
+    # verify
+    assert_equal "/products", page.current_path
+    assert_equal product_count_before, Product.count
   end
 end
